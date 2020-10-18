d136fb1ce5a1a1992c465696fd784ff90b7f04b7
dqhan <291927006@qq.com>
2020-10-19 00:26:31 +0800

    library

--- component/Home/index.jsx
+++ component/Home/index.jsx
@@ -1,7 +1,6 @@
 import Introduction from '../Introduction';
 import Member from '../Member';
 import Button from '../Button';
-import Test from '../button.vue';
 function Home() {
 
     const [key, setKey] = React.useState("2");
@@ -24,7 +23,7 @@ function Home() {
             case "5":
                 return <div>未完成</div>;
             case "6":
-                return <Test />;
+                return <Button />;
             case "7":
                 return <div>未完成</div>;
             case "8":
